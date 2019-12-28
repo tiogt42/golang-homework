e7e3bf1235e99e1dfc0f26c36e327c9520989fbd
tiogt42 <39779600+tiogt42@users.noreply.github.com>
2019-12-28 10:25:35 +0900

    5dz

--- dz5.go
+++ dz5.go
@@ -5,39 +5,42 @@ import (
 )
 
 func main() {
-	a := []int{1, 2, 2, 2, 2}
+	a := []int{1, 2, 3, 4, 2 ,1,2,3,4,2,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3}
 
-	unique_count(&a)
-	fmt.Println(unique_count(&a))
+	Unique_count(&a)
+	fmt.Println(Unique_count(&a))
 }
 
 
-func unique_count (a *[]int) int{
-	b := 0
-	v := 0
-	c := 0
-	d := 0
-	g := 0
-	for i  := 0; i < len(*a); i++ {
-		for j := i; j < len(*a); j++ {
-			if (*a)[i] < (*a)[j] {
 
 
-				c = c+1
-				d = c
-			}
 
-
-			if (*a)[i] > (*a)[j] {
-
-
-				v = v+1
-				g = v
-			}
+func Unique_count (a*[] int) int{
+	var sum_uniq int = 1
+	bubble_Sort(a)
+	for i:=1; i<len(*a); i++{
+		if (*a)[i-1] != (*a)[i]{
+			sum_uniq++
 		}
 	}
-	b = d+g
-	return b
+	return sum_uniq
+
+}
+
+
+func bubble_Sort (a *[]int) {
+
+	for i := 0; i < len(*a); i++ {
+		for j := i; j < len(*a); j++ {
+			if (*a)[i] > (*a)[j] {
+
+				swap := (*a)[i]
+				(*a)[i] = (*a)[j]
+				(*a)[j] = swap
+			}
+		}
+
+	}
 }
 
 
